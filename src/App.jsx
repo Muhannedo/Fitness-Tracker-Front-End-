71b8b608a8fc0673bcc0d33bf0b28c0a6bd3fc35
Muhannad <muhanned9980@gmail.com>
2024-12-05 13:26:05 +0300

    init setup

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+// App.jsx
+
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar/NavBar";
+import Landing from "./components/Landing/Landing";
+import Dashboard from "./components/Dashboard/Dashboard";
+import SignupForm from "./components/SignupForm/SignupForm";
+import SigninForm from "./components/SigninForm/SigninForm";
+
+const App = () => {
+  const [user, setUser] = useState(null);
+
+  return (
+    <>
+      <NavBar user={user} />
+      <Routes>
+        {user ? (
+          <Route path="/" element={<Dashboard user={user} />} />
+        ) : (
+          <Route path="/" element={<Landing />} />
+        )}
+        <Route path="/signup" element={<SignupForm setUser={setUser} />} />
+        <Route path="/signin" element={<SigninForm setUser={setUser} />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
